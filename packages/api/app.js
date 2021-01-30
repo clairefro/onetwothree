457348ed4d2f6ff7d98749bbd6ed16db3da20f10
Claire Froelich <claire.froelich@gmail.com>
2021-01-30 13:30:11 -0800

    Add working mongodb connection, refactor controller methods

--- packages/api/app.js
+++ packages/api/app.js
@@ -3,10 +3,26 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const { verify } = require("./utils/verify");
+const mongoose = require("mongoose");
+const { login, logout } = require("./controllers");
 
 const port = process.env.PORT || 4444;
 
-const { login, logout } = require("./routes/auth");
+// // super not secure
+// const userSchema = new mongoose.Schema({
+// 	username: String,
+// 	password: String,
+// });
+
+// const User = mongoose.model("User", userSchema);
+
+// (async () => {
+// 	const randy = new User({ usrename: randy, password: "tegridy" });
+// 	await randy.save().catch((err, user) => {
+// 		console.log("Failed to save user ", user.username);
+// 	});
+// })();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
@@ -19,6 +35,18 @@ app.post("/am-i-in", verify, (req, res) => {
 
 // app.get("/leaderboard", routeHandler);
 
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
-});
+// connect to db and start server
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("MongoDB Connected...");
+	})
+	.then(() => {
+		app.listen(port, () => {
+			console.log(`Example app listening at http://localhost:${port}`);
+		});
+	})
+	.catch((e) => console.log(e));
---
+++ packages/api/controllers/index.js
@@ -0,0 +1,6 @@
+const { login, logout } = require("./auth");
+
+module.exports = {
+	login,
+	logout,
+};
