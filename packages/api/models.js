91178e3df05f9a223737615fbc3d9e0b63e84605
Claire Froelich <claire.froelich@gmail.com>
2021-01-31 15:04:52 -0800

    Add score schema to backend

--- packages/api/models.js
+++ packages/api/models.js
@@ -13,3 +13,20 @@ const User = mongoose.model("User", userSchema);
 module.exports = {
 	User,
 };
+
+const scoreSchema = new mongoose.Schema(
+	{
+		username: { type: String, required: true },
+		lang: { type: String, required: true },
+		score: { type: String, required: true },
+		streak: { type: String, required: true },
+	},
+	{ timestamps: true }
+);
+
+const Score = mongoose.model("Score", scoreSchema);
+
+module.exports = {
+	User,
+	Score,
+};
